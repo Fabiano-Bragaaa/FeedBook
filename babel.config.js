04bf52d6a5df1d2c9632131522a8066caed01799
Fabiano Braga <fabianobragadev@gmail.com>
2025-03-20 12:27:57 -0300

    feature: add eslint, husk and prettier in project

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+    ['@babel/plugin-transform-private-methods', {loose: true}],
+    ['@babel/plugin-transform-class-properties', {loose: true}],
+    ['@babel/plugin-transform-private-property-in-object', {loose: true}],
+  ],
 };
---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,9 @@
+import {View, Text as RNText} from 'react-native';
+
+export function Text() {
+  return (
+    <View>
+      <RNText>Ola</RNText>
+    </View>
+  );
+}
